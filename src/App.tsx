400fa8544b6cf50aacc278ee468ac7330623cad5
Marcus Davanco <md.daves@gmail.com>
2022-03-02 08:39:58 -0300

    feat: Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { GlobalStyles } from "./styles/global";
+import { Header } from "./styles/Header";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <GlobalStyles />
-    </div>
+    </>
   );
 }
