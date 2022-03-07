b680776cae844bd85ad46a889199702f718dfa87
Marcus Davanco <md.daves@gmail.com>
2022-03-07 12:18:50 -0300

    feat: Criando hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,12 @@
-import { useState, useContext, FormEvent } from "react";
+import { useState, FormEvent } from "react";
 import Modal from "react-modal";
-import { TransactionsContext } from "../../TransactionsContext";
 
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { useTransactions } from "../../hooks/useTransactions";
 
 Modal.setAppElement("#root");
 
@@ -19,7 +19,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
